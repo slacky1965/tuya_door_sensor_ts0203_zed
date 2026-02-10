d5673053270cf0d20b9b294168e40904debf595c
Fedor <lfa1@yandex.ru>
2026-02-10 22:15:47 +0300

    Update win_make.cmd

--- win_make.cmd
+++ win_make.cmd
@@ -1,7 +1,3 @@
 make clean
-make clean-bin
-make PROJECT_NAME="tuya_door_sensor_0x20_zed" PROJECT_DEF="-DBOARD=BOARD_TS0203_ZBEACON"
-
-make clean
-make PROJECT_NAME="tuya_door_sensor_0x26_zed" PROJECT_DEF="-DBOARD=BOARD_TS0203_TZ3000_hufxidjp"
-
+make PROJECT_MODEL=_model1
+make PROJECT_MODEL=_model2
